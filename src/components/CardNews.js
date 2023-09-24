a69d453684d7790bb20c44bc8de62416136b2563
Natalia Graciano <nataliavgraciano@gmail.com>
2023-09-24 15:52:08 -0300

    building a component

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -1,10 +1,14 @@
-class CardNews extends HTMLElement {
-    constructor() {
-        super()
+class Cardnews extends HTMLElement {
+    constructor(){
+        super();
 
-        const shadow = this.attachShadow({ mode: "open" })
-        shadow.innerHTML = "<h1>Hello, Natalia!</h1>"
+        const shadow = this.attachShadow({mode: "open"});
+        shadow.innerHTML = "<h1></h1>"
     }
+
+    build(){}
+
+    styles(){}
 }
 
-customElements.define('card-news', CardNews)
+customElements.define("card-news", Cardnews);
