2fff2f1a30ebbc1b1c34dd8c098fdee2e5d2f60b
Natalia Graciano <nataliavgraciano@gmail.com>
2023-09-24 16:11:41 -0300

    setting class properties

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -1,14 +1,30 @@
 class Cardnews extends HTMLElement {
-    constructor(){
-        super();
+  constructor() {
+    super();
 
-        const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1></h1>"
-    }
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.styles());
+  }
 
-    build(){}
+  build() {
+    const componentRoot = document.createElement("div");
+    componentRoot.setAttribute("class", "card");
 
-    styles(){}
+    const cardLeft = document.createElement("div");
+    cardLeft.setAttribute("class", "card__left");
+    
+    const cardRight = document.createElement("div");
+    cardRight.setAttribute("class", "card__right");
+
+
+    componentRoot.appendChild(cardLeft);
+    componentRoot.appendChild(cardRight);
+
+    return componentRoot;
+  }
+
+  styles() {}
 }
 
 customElements.define("card-news", Cardnews);
